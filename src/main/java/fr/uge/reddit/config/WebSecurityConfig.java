253857c810d61acd05e6da30d204f670b07063bf
MohamedYassineYAHYAOUI <yahyaouiy1997@gmail.com>
2022-01-26 13:13:39 +0100

    Conflicts

--- src/main/java/fr/uge/reddit/config/WebSecurityConfig.java
+++ src/main/java/fr/uge/reddit/config/WebSecurityConfig.java
@@ -56,5 +56,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
     }
+
    }
 
